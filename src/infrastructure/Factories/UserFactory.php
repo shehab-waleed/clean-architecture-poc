c139b5b14b6457a9979afd4b68a72d13c24d2e54
shehabwaleed <shehabwaleed2010@gmail.com>
2025-10-15 01:57:15 +0300

    feat: Push factories into infrastructure layer

---
+++ src/infrastructure/Factories/UserFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Src\infrastructure\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Model>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => \Illuminate\Support\Str::random(10),
+        ];
+    }
+}
